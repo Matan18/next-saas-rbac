1a64ae3dc0bfbddd24e5b38276486880a6c99615
Mateus Andriola <mateusandriola18@gmail.com>
2024-11-02 16:32:44 -0300

    config: setup database schema & seed

---
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -0,0 +1,45 @@
+import type { FastifyInstance } from "fastify";
+import type { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+
+export async function createAccount(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/users",
+    {
+      schema: {
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          password: z.string().min(6),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { email, name, password } = request.body;
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: "User with this e-mail already exists",
+        });
+      }
+
+      const passwordHash = await hash(password, 6);
+
+      const user = await prisma.user.create({
+        data: {
+          email,
+          name,
+          passwordHash: passwordHash,
+        },
+      });
+
+      reply.status(201).send(user);
+    }
+  );
+}
--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor, projectSchema } from "@saas/auth";
-
-const ability = defineAbilityFor({ role: "MEMBER", id: "user-id" });
-
-const project = projectSchema.parse({ id: "project-id", ownerId: "user-id" });
-
-const userCanInviteSomeoneElse = ability.can("get", "User");
-
-console.log("hello", ability.can("delete", project));
