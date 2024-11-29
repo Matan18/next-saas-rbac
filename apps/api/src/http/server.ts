c2765889185f0c060bdbdedec553867b36761a1c
Mateus Andriola <mateusandriola18@gmail.com>
2024-11-29 19:17:25 -0300

    route: get organization invites

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -34,6 +34,7 @@ import { updateMember } from "./routes/members/update-member";
 import { removeMember } from "./routes/members/remove-member";
 import { createInvite } from "./routes/invites/create-invite";
 import { getInvite } from "./routes/invites/get-invite";
+import { getInvites } from "./routes/invites/get-invites";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -95,6 +96,7 @@ app.register(removeMember);
 
 app.register(createInvite);
 app.register(getInvite);
+app.register(getInvites);
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log("Server started at http://localhost:3333");
