96bd65bd82bb70da9c2693e96b5490fa355174ea
Mateus Andriola <mateusandriola18@gmail.com>
2024-12-16 19:02:16 -0300

    refine sign-in page

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,14 +1,43 @@
 'use server'
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
+import { HTTPError } from 'ky'
+import { z } from 'zod'
 
-export async function signInWithEmailAndPassword(data: FormData) {
-  const { email, password } = Object.fromEntries(data)
+const signInSchema = z.object({
+  email: z.string().email('Pleas, provide a valid e-mail address.'),
+  password: z.string().min(1, 'Please, provide a password.'),
+})
 
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+export async function signInWithEmailAndPassword(
+  previousState: unknown,
+  data: FormData
+) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
 
-  console.log(result)
+  try {
+    if (!result.success) {
+      const errors = result.error.flatten().fieldErrors
+
+      return { success: false, message: null, errors }
+    }
+
+    const { token } = await signInWithPassword(result.data)
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in few minutes.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
---
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,81 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+import { AlertTriangle, Loader2 } from 'lucide-react'
+import githubIcon from '@/assets/github-icon.svg'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { signInWithEmailAndPassword } from './actions'
+import { useActionState } from 'react'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+
+export function SignInForm() {
+  const [state, formAction, isPending] = useActionState(
+    signInWithEmailAndPassword,
+    { success: false, message: null, errors: null }
+  )
+  const { success, message, errors } = state
+
+  return (
+    <form action={formAction} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Sign in failed</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
+      <div className="space-y-1">
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" type="email" id="email" />
+        {errors?.email && (
+          <p className="text-sm font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
+      </div>
+      <div className="space-y-1">
+        <Label htmlFor="password">Password</Label>
+        <Input name="password" type="password" id="password" />
+        {errors?.password && (
+          <p className="text-sm font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
+        <Link
+          href="/auth/forgot-password"
+          className="text-sm font-medium text-foreground hover:underline"
+        >
+          Forgot your password?
+        </Link>
+      </div>
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {isPending ? (
+          <Loader2 className="size-4 animate-spin" />
+        ) : (
+          'Sign in with e-mail'
+        )}
+      </Button>
+
+      <Button type="button" variant="link" className="w-full" size="sm" asChild>
+        <Link href="/auth/sign-up">Create new account</Link>
+      </Button>
+
+      <Separator />
+
+      <Button variant="outline" className="w-full">
+        <Image
+          src={githubIcon}
+          className="mr-2 size-4 dark:invert"
+          alt="Github icon"
+        />
+        Sign in with Gitthub
+      </Button>
+    </form>
+  )
+}
