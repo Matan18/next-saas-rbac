c1c065d8572dad5e745b7a4ac8c80e0b71bad560
Mateus Andriola <mateusandriola18@gmail.com>
2025-01-27 18:51:23 -0300

    organization-switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,6 +1,8 @@
 import githubIcon from '@/assets/github-icon.svg'
 import Image from 'next/image'
+import { Slash } from 'lucide-react'
 import { ProfileButton } from './profile-button'
+import { OrganizationSwitcher } from './organization-switcher'
 
 export function Header() {
   return (
@@ -11,6 +13,9 @@ export function Header() {
           alt="Github Icon"
           className="size-6 dark:invert"
         />
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
