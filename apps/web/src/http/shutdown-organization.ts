14d38c0e3bd1786fa82c0a6822adaffb2de81c85
Mateus Andriola <mateusandriola18@gmail.com>
2025-06-28 10:10:13 -0300

    settings page + shutdown organization

---
+++ apps/web/src/http/shutdown-organization.ts
@@ -0,0 +1,13 @@
+import { api } from './api-client'
+
+interface ShutdownOrganizationRequest {
+  org: string
+}
+
+export async function shutdownOrganization({
+  org,
+}: ShutdownOrganizationRequest) {
+  await api.delete(`organizations/${org}`, {
+    json: { org },
+  })
+}
