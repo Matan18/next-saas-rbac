30c48636f38e5f3a1829747c2da9332aee8efa27
Mateus Andriola <mateusandriola18@gmail.com>
2025-07-03 09:14:28 -0300

    start invite page

--- apps/web/src/http/get-invites.ts
+++ apps/web/src/http/get-invites.ts
@@ -1,10 +1,12 @@
+import type { Role } from '@saas/auth'
+
 import { api } from './api-client'
 
 interface GetInvitesResponse {
   invites: {
     id: string
     createdAt: string
-    role: 'ADMIN' | 'MEMBER' | 'BILLING'
+    role: Role
     email: string
     author: {
       name: string | null
